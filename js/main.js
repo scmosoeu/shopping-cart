861ff896023898634fef413c542b7b5e69ee0874
Selebogo Mosoeu <scmosoeu@gmail.com>
2023-07-01 09:23:57 +0200

    added year in footer

--- js/main.js
+++ js/main.js
@@ -1,20 +1,29 @@
-let jsonData = "../data/products.json"
+/********************** SET CURRENT IN THE FOOTER ****************/
+// Insert year on the footer of the html
+const year = document.getElementById("year");
+const currentYear = new Date()
+year.innerText = currentYear.getFullYear();
 
+/********************* JSON DATA INITIAL INGESTION ***************/
+
+const jsonData = "../data/products.json"
 let dropDownItem = document.getElementById("dropdown-items")
 
+// Insert items in the Drop-Down Menu
+function initialDropdownItems(items) {
+  items.forEach(item => {
+    // Get Item name from name JSON field
+    const itemName = item.name.toLowerCase();
+    // Create option element
+    const node = document.createElement("option");
+    node.innerText = item.name;
+    // Create Element Attributes
+    node.setAttribute("value", itemName);
+    dropDownItem.append(node)
+  })
+}
 
 fetch(jsonData)
-    .then((response) => response.json())
-    .then((data) => {
-        data.forEach(item => {
-            const itemValue = String(item.name).toLowerCase();
-            // Create a label
-            const node = document.createElement("option");
-            const textnode = document.createTextNode(item.name);
-            node.appendChild(textnode)
-            node.setAttribute("value", itemValue)
-
-            dropDownItem.appendChild(node)
-        })
-    })
-    .catch((err) => console.log(err))
+  .then(response => response.json())
+  .then(data => initialDropdownItems(data))
+  .catch(err => console.log(err))
